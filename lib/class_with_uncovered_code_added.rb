259f3ce56a3832c7565aa918df296554276b4b85
Rob Esris <resris@paperlesspost.com>
2016-07-25 15:08:32 -0400

    Add existing uncovered method

--- lib/class_with_uncovered_code_added.rb
+++ lib/class_with_uncovered_code_added.rb
@@ -5,6 +5,12 @@ class ClassWithUncoveredCodeAdded
     1 # arbitrary return value
   end
 
+  def existing_uncovered_method
+    puts "This method is uncovered by tests"
+    puts "But it is preexisting, not introduced in the 'new' my_branch branch"
+    puts "Therefore, smackdown should not call it out"
+  end
+
   # This method will be "added" (really just uncommented) in the 'my_changes' branch of the repo,
   # So that when we compare it to master, we should see it as new code.  In this case, we WON'T add
   # a test for it and will expect smackdown to call us out.
